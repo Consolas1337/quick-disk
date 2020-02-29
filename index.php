2cc9970c552bb7b8e9a8e8c3a720d4f7460e5d96
Maxim Sandakov <sandakov2013@yandex.ru>
2020-02-29 16:06:22 +0500

    +logo +parse profile image.merge vue register app

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <html lang="ru">
 <head>
-<meta charset="UTF-8">
-<title>Consolas disk</title>
-<link rel="stylesheet" href="css/style.css">
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-<script src="https://vk.com/js/api/openapi.js?167" type="text/javascript" SameSite="None Secure"></script>
+    <meta charset="UTF-8">
+    <title>Quick disk by Consolas</title>
+    <link rel="icon" href="css/png/disk1.png">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://vk.com/js/api/openapi.js?167" type="text/javascript" SameSite="None Secure"></script>
 </head>
 <body id="body">
+<header>
+    <div class="logo-container">
+        <div class="logo-body">
+            <a class="logo-text">Quick disk</a>&nbsp;<a href="https://vk.com/consolas" class="logo-link">by Consolas</a>
+        </div>
+    </div>
     <div class="auth-container" id="auth">
         <div class="auth-body">
-            <a v-if="!data.name">Private password: </a><input type="password" v-if="!data.name" placeholder="password here" v-model="password"></input>
+            <a v-if="!data.name"></a><input type="password" v-if="!data.name" placeholder="password here" v-model="password"></input>   
+            <input type="button" value="Register" v-on:click="register()" v-if="password && !data.role">
             <br><a v-if="data.name">Welcome {{ data.name }}</a>
         </div>
     </div>
-    <div  id="register">
-        <input type="text" name="pass" id="pass" v-model="newPassword" placeholder="Your quick password">
-        <input type="button" value="Register" v-on:click="register()">
-    </div>
+</header>
     <script>
     new Vue({
     el: "#auth",
+    name: "loginApp",
     watch: {
         password: function(newPass, oldPass) {
             // todo: add loading icon
@@ -34,6 +40,7 @@
     data: {
         password: '',
         request: {},
+        profilePhotoUrl: '',
         data: {
             name: '',
         },
@@ -48,6 +55,24 @@
                 .then(response => this.loadUserData(response))
                 .catch(error => console.log(error));
         },
+        register: function() {
+            VK.Auth.login((vkResponse) => {
+                if (vkResponse.session) {
+                    VK.Api.call('users.get', {user_ids: vkResponse.session.mid, v:"5.103", fields:"photo_200"}, (r) => {
+                        if(r.response) {
+                            this.profilePhotoUrl = r.response[0].photo_200;
+                            let incomingData = new URLSearchParams({new_password: this.password, profilePhotoUrl: this.profilePhotoUrl, session: JSON.stringify(vkResponse.session)});
+                            axios
+                                .post('./register.php',incomingData)
+                                .then(response => console.log(response.data))
+                                .catch(error => console.log(error));
+                        }
+                    });
+                } else {
+                    alert("Для регистрации нужно авторизироваться с помощью VK. Без этого никак.");
+                }
+            });
+        },
         loadUserData: function(response) {
             this.data = response.data;
             if (this.data.error===undefined) {
@@ -58,41 +83,14 @@
         },
     },
     mounted() {
+        VK.init({apiId: 7331619,});
         if (localStorage.name & localStorage.token) {
             console.log("You logged in");
         }
-    },
+    }, 
     });
     </script>
-    <script>
-    new Vue({
-    el: "#register",
-    data: {
-        newPassword: '',
-    },
-    methods: {
-        register: function() {
-            let parentThis = this;
-            VK.Auth.login(function(vkResponse) {
-            if (vkResponse.session) {
-                let incomingData = new URLSearchParams({new_password: parentThis.newPassword, session: JSON.stringify(vkResponse.session)});
-                axios
-                    .post('./register.php',incomingData)
-                    .then(response => console.log(response.data))
-                    .catch(error => console.log(error));
-            } else {
-                alert("Для регистрации нужно авторизироваться с помощью VK. Без этого никак.");
-            }
-            });
-        },
-    },
-    mounted() {
-        VK.init({
-            apiId: 7331619,
-        });
-    },
-    });
-    </script>
+
 </body>
 </html>
 
--- register.php
+++ register.php
@@ -11,7 +11,7 @@ header('Content-Type: application/json');
 if (!isset($_POST['new_password']) and !isset($_POST['session'])) {
     sendError("Need 'new_password' and 'session' values!");
 }
-
+echo json_encode($_POST);
 $session = json_decode($_POST['session']);
 $userPass = $_POST['new_password'];
 
@@ -32,12 +32,13 @@ $first_name = $mysqli->real_escape_string($session->user->first_name);
 $last_name = $mysqli->real_escape_string($session->user->last_name);
 $user_id = $mysqli->real_escape_string($session->user->id);
 $userPass = $mysqli->real_escape_string($userPass);
+$userImage = $mysqli->real_escape_string($_POST['profilePhotoUrl']);
 $folder = $user_id;
 
 $result = $mysqli->query("SELECT id FROM `users` WHERE vk_id='".$user_id."'");
 
 if ($result->num_rows === 0 or !$result) {
-    $result = $mysqli->query("INSERT INTO `users` SET name='".$first_name."',last_name='".$last_name."',vk_id='".$user_id."',folder='".$folder."',password='".$userPass."'");
+    $result = $mysqli->query("INSERT INTO `users` SET name='".$first_name."',last_name='".$last_name."',image_url='".$userImage."',vk_id='".$user_id."',folder='".$folder."',password='".$userPass."'");
 } else {
     sendError("This account already registered! Try with another account.");
 }
